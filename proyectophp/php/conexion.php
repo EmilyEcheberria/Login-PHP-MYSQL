bb85c8657209140bc052227942194ff81e00a79b
Emily <64603938+EmilyEcheberria@users.noreply.github.com>
2024-01-15 07:42:09 -0300

    Add files via upload

---
+++ proyectophp/php/conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+ $conexion= mysqli_connect("localhost", "root", "", "loguin_register_db");
+
+ /*
+ if($conexion){
+    echo 'Conectado exitosamente a la Base de Datos';
+ }
+ else{
+    echo 'No se ha podido conectar a la Base de Datos';
+ }
+*/
+?>
